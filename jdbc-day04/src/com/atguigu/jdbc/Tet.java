46c6f23ffac7e735a79c6b0d58941c0dd2e11664
Layne <1467824792@qq.com>
2023-03-21 16:37:33 +0800

    date1

---
+++ jdbc-day04/src/com/atguigu/jdbc/Tet.java
@@ -0,0 +1,13 @@
+package com.atguigu.jdbc;
+
+/**
+ * @author lystart
+ * @create 2023-03-21 16:36
+ */
+public class Tet {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
